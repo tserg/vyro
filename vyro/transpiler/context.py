3394333d1f52af070c2db3d38dbf37c6e2b26f3d
tserg <8017125+tserg@users.noreply.github.com>
2022-07-30 12:49:50 +0800

    fix: largest node id for context

--- vyro/transpiler/context.py
+++ vyro/transpiler/context.py
@@ -1,6 +1,22 @@
 from vyper import ast as vy_ast
 
 
+def _get_largest_node_id(d: dict):
+    """
+    Walks the AST dict to get the largest `node_id`.
+    """
+    for k, v in d.items():
+        if k == "node_id":
+            yield v
+        elif isinstance(v, dict):
+            for i in _get_largest_node_id(v):
+                yield i
+        elif isinstance(v, list):
+            for i in v:
+                if isinstance(i, dict):
+                    for j in _get_largest_node_id(i):
+                        yield j
+
 class ASTContext:
     def __init__(self):
         self.last_id = 0
@@ -9,9 +25,14 @@ class ASTContext:
     def get_context(cls, vyper_module: vy_ast.Module) -> "ASTContext":
         ctx = cls()
 
-        # Get the last node
-        last_child = vyper_module.get_descendants(reverse=False).pop()
-        ctx.last_id = last_child.node_id
+        ast_dict = vyper_module.to_dict()
+
+        largest = 0
+        for node_id in _get_largest_node_id(ast_dict):
+            if node_id > largest:
+                largest = node_id
+
+        ctx.last_id = largest
         return ctx
 
     def reserve_id(self):
