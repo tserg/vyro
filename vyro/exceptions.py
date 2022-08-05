829c4c03ec030bea52eabde7800f79a44227fd4e
tserg <8017125+tserg@users.noreply.github.com>
2022-08-05 22:39:08 +0800

    revert exception constructor

--- vyro/exceptions.py
+++ vyro/exceptions.py
@@ -3,7 +3,7 @@ class VyroException(Exception):
     Base exception class
     """
 
-    def __init__(self, message="Error Message not found.", *items):
+    def __init__(self, message="Error Message not found.", node=None):
         """
         Exception initializer.
 
@@ -11,31 +11,14 @@ class VyroException(Exception):
         ---------
         message : str
             Error message to display with the exception.
-        *items : VyperNode | Tuple[str, VyperNode], optional
-            Vyper ast node(s), or tuple of (description, node) indicating where
-            the exception occured. Source annotations are generated in the order
-            the nodes are given.
-
-            A single tuple of (lineno, col_offset) is also understood to support
-            the old API, but new exceptions should not use this approach.
+        *items : VyperNode
+            Vyper ast node(s) indicating where the exception occured.
+            Source annotations are generated in the order the nodes are given.
         """
         self.message = message
-        self.lineno = None
-        self.col_offset = None
-        self.annotations = items
 
     def __str__(self):
-
-        if not hasattr(self, "annotations"):
-            if self.lineno is not None and self.col_offset is not None:
-                return f"line {self.lineno}:{self.col_offset} {self.message}"
-            else:
-                return self.message
-
-        annotation_list = []
-        annotation_msg = "\n".join(annotation_list)
-
-        return f"{self.message}\n{annotation_msg}"
+        return f"\nTranspilation failed with the following error:\n{self.message}\n"
 
 
 class UnsupportedNode(VyroException):
