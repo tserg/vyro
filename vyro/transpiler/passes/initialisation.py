9058b93c60cfd794811c67fbdf013599df143392
tserg <8017125+tserg@users.noreply.github.com>
2022-08-29 22:29:27 +0800

    reassign RHS of AnnAssign

--- vyro/transpiler/passes/initialisation.py
+++ vyro/transpiler/passes/initialisation.py
@@ -1,16 +1,52 @@
 from vyper import ast as vy_ast
 
 from vyro.transpiler.context import ASTContext
-from vyro.transpiler.utils import get_cairo_type, initialise_function_implicits
+from vyro.transpiler.utils import (
+    generate_name_node,
+    get_cairo_type,
+    get_scope,
+    initialise_function_implicits,
+    insert_statement_before,
+    set_parent,
+)
 from vyro.transpiler.visitor import BaseVisitor
 
 
 class InitialisationVisitor(BaseVisitor):
     def visit_AnnAssign(self, node: vy_ast.AnnAssign, ast: vy_ast.Module, context: ASTContext):
+        # Move RHS into a local variable.
+        # Since `AnnAssign` is a `tempvar`, it cannot be assigned to a function call.
+        # Therefore, we re-assign the RHS to a temporary local variable.
+        temp_name_node = generate_name_node(context.reserve_id())
+
+        rhs = node.value
+        node._children.remove(node.value)
+
+        temp_assign_node = vy_ast.Assign(
+            node_id=context.reserve_id(),
+            targets=[temp_name_node],
+            value=rhs,
+            ast_type="Assign",
+        )
+        set_parent(rhs, temp_assign_node)
+        set_parent(temp_name_node, temp_assign_node)
+
+        # Insert `Assign` before `AnnAssign`
+        scope_node, scope_body = get_scope(node)
+        insert_statement_before(temp_assign_node, node, scope_node, scope_body)
+
+        # Replace RHS with temp name node
+        temp_name_node_dup = generate_name_node(context.reserve_id(), name=temp_name_node.id)
+        node.value = temp_name_node_dup
+        set_parent(temp_name_node_dup, node)
+
         typ = node.target._metadata.get("type") or node._metadata.get("type")
         if typ:
             cairo_typ = get_cairo_type(typ)
             node._metadata["type"] = cairo_typ
+            temp_assign_node._metadata["type"] = cairo_typ
+            temp_name_node._metadata["type"] = cairo_typ
+            temp_name_node_dup._metadata["type"] = cairo_typ
 
     def visit_AugAssign(self, node: vy_ast.AnnAssign, ast: vy_ast.Module, context: ASTContext):
         typ = node.target._metadata.get("type")
@@ -21,7 +57,9 @@ class InitialisationVisitor(BaseVisitor):
         # Initialise implicits
         initialise_function_implicits(node)
 
-        for i in node.body:
+        to_visit = node.get_descendants((vy_ast.AnnAssign, vy_ast.AugAssign))
+
+        for i in to_visit:
             self.visit(i, ast, context)
 
     def visit_Module(self, node: vy_ast.Module, ast: vy_ast.Module, context: ASTContext):
