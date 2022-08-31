dde99d6fce18b8377e81401958036d60a80781ea
tserg <8017125+tserg@users.noreply.github.com>
2022-08-31 22:38:47 +0800

    handle immutables

--- vyro/transpiler/passes/constructor_handler.py
+++ vyro/transpiler/passes/constructor_handler.py
@@ -1,3 +1,7 @@
+from vyper import ast as vy_ast
+
+from vyro.cairo.types import FeltDefinition
+from vyro.transpiler.utils import generate_name_node, get_cairo_type, set_parent
 from vyro.transpiler.visitor import BaseVisitor
 
 
@@ -11,3 +15,75 @@ class ConstructorHandler(BaseVisitor):
 
         # Change `__init__` to `constructor`
         node.name = "constructor"
+
+        # Filter for immutable declarations and visit
+        assign_children = [
+            n for n in node.get_descendants(vy_ast.Assign) if isinstance(n.target, vy_ast.Name)
+        ]
+
+        for c in assign_children:
+            self.visit(c, ast, context)
+
+    def visit_Assign(self, node, ast, context):
+        # Check if it is an immutable
+        varname = node.target.id
+        is_immutable = ast.get_descendants(
+            vy_ast.VariableDecl, {"is_immutable": True, "target.id": varname}
+        )
+
+        if len(is_immutable) == 0:
+            return
+
+        # Get type
+        var_decl = is_immutable.pop()
+        vy_typ = var_decl._metadata["type"]
+        cairo_typ = get_cairo_type(vy_typ)
+
+        # Update type of immutable declaration
+        var_decl._metadata["type"] = cairo_typ
+
+        # Transform LHS of assignment to immutable variable into `self.varname`
+        node._children.remove(node.target)
+
+        self_node = generate_name_node(context.reserve_id(), name="self")
+        self_node._metadata["type"] = FeltDefinition()
+
+        attribute_node = vy_ast.Attribute(
+            node_id=context.reserve_id(), attr=varname, value=self_node, ast_type="Attribute"
+        )
+        attribute_node._metadata["type"] = cairo_typ
+
+        set_parent(self_node, attribute_node)
+
+        node.target = attribute_node
+        set_parent(attribute_node, node)
+
+        # Replace subsequent references of immutable variable with `self.varname`
+        # to make use of the storage var pass to read from storage
+
+        contract_fns = ast.get_descendants(vy_ast.FunctionDef)
+
+        for fn in contract_fns:
+            fn_typ = fn._metadata.get("type")
+
+            # Skip constructor because it has been handled
+            if fn_typ.is_constructor:
+                continue
+
+            immutable_references = fn.get_descendants(vy_ast.Name, {"id": varname})
+
+            for i in immutable_references:
+                self_node = generate_name_node(context.reserve_id(), name="self")
+                self_node._metadata["type"] = FeltDefinition()
+
+                attribute_node = vy_ast.Attribute(
+                    node_id=context.reserve_id(),
+                    attr=varname,
+                    value=self_node,
+                    ast_type="Attribute",
+                )
+                attribute_node._metadata["type"] = cairo_typ
+
+                set_parent(self_node, attribute_node)
+
+                ast.replace_in_tree(i, attribute_node)
--- vyro/transpiler/passes/unsupported.py
+++ vyro/transpiler/passes/unsupported.py
@@ -1,8 +1,5 @@
-from vyro.exceptions import UnsupportedFeature
 from vyro.transpiler.visitor import BaseVisitor
 
 
 class UnsupportedVisitor(BaseVisitor):
-    def visit_VariableDecl(self, node, ast, context):
-        if node.is_immutable:
-            raise UnsupportedFeature("Immutables are not supported in Cairo", node)
+    pass
