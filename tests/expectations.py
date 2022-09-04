96924396df353e8907dc850e3886726c64658c02
tserg <8017125+tserg@users.noreply.github.com>
2022-09-05 01:36:45 +0800

    add tests

--- tests/expectations.py
+++ tests/expectations.py
@@ -139,6 +139,11 @@ EXPECTATIONS = [
             ("xor_uint256", [[150, 200], 94], [[150, 200], 94]),
         ),
     ),
+    ("binop_nested_arithmetic_int128", (("add_mul_int128", [[100, 9], 101], [[100, 9], 101]),)),
+    (
+        "binop_nested_arithmetic_uint256",
+        (("pow_mul_uint256", [[100, 18], 100 * 10**18], [[100, 18], 100 * 10**18]),),
+    ),
     (
         "boolop",
         (
@@ -151,10 +156,13 @@ EXPECTATIONS = [
             ("bool_or", [[False, False], False], [[0, 0], 0]),
         ),
     ),
-    ("binop_nested_arithmetic_int128", (("add_mul_int128", [[100, 9], 101], [[100, 9], 101]),)),
     (
-        "binop_nested_arithmetic_uint256",
-        (("pow_mul_uint256", [[100, 18], 100 * 10**18], [[100, 18], 100 * 10**18]),),
+        "block_number",
+        (("get_block_no", [[], None], [[], None]),),
+    ),
+    (
+        "block_timestamp",
+        (("get_timestamp", [[], None], [[], None]),),
     ),
     (
         "compare_int128",
