dde8dead513ade363728941afe3711d10a9a480c
tserg <8017125+tserg@users.noreply.github.com>
2022-09-05 18:06:47 +0800

    package builtin constants passes

---
+++ vyro/transpiler/passes/builtin_constants/builtin_constant_handler.py
@@ -0,0 +1,19 @@
+from vyper import ast as vy_ast
+
+from vyro.transpiler.context import ASTContext
+from vyro.transpiler.passes.builtin_constants.block_constant_handler import (
+    BlockConstantHandlerVisitor,
+)
+from vyro.transpiler.passes.builtin_constants.msg_sender_converter import MsgSenderConverterVisitor
+from vyro.transpiler.visitor import BaseVisitor
+
+PASSES = [
+    MsgSenderConverterVisitor(),
+    BlockConstantHandlerVisitor(),
+]
+
+
+class BuiltinConstantHandlerVisitor(BaseVisitor):
+    def visit_Module(self, node: vy_ast.Module, ast: vy_ast.Module, context: ASTContext):
+        for v in PASSES:
+            v.visit(node, ast, context)
