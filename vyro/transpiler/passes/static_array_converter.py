82c30d2d174b8b65fdeaebce308091845ec540de
tserg <8017125+tserg@users.noreply.github.com>
2022-11-25 16:35:28 +0800

    add comment

--- vyro/transpiler/passes/static_array_converter.py
+++ vyro/transpiler/passes/static_array_converter.py
@@ -7,6 +7,13 @@ from vyro.transpiler.visitor import BaseVisitor
 
 
 class StaticArrayConverterVisitor(BaseVisitor):
+    """
+    This pass converts static arrays into Cairo storage mappings.
+
+    For example, reading `array[i][j]` is equivalent to `array_storage.read(i, j)`,
+    and writing `array[i][j] = k` is equivalent to `array_storage.write(i, j, k)`.
+    """
+
     def visit_VariableDecl(self, node: vy_ast.VyperNode, ast: vy_ast.Module, context: ASTContext):
         vy_typ = node._metadata.get("type")
         if not isinstance(vy_typ, ArrayDefinition):
